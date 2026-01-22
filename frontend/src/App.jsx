4df219d218f7824b5039f467cdbdf8b6fb352317
JulieKihumba <eng.juliekihumba@gmail.com>
2026-01-22 18:24:57 +0300

    `Added react-router-dom and updated dependencies in package-lock.json and package.json`

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Destinations from "./pages/Destinations";
+import TripTypes from "./pages/TripTypes";
+import Blog from "./pages/Blog";
+import Contact from "./pages/Contact";
 
 const App = () => {
-  console.log("Hello to World");
   return (
-    <div>
-      <p>Hello World</p>
-    </div>
-  )
-}
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/destination" element={<Destinations />} />
+        <Route path="/trip-type" element={<TripTypes />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
